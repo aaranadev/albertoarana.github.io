e501b08e39198015e4f42d15c6b68521138aa02a
Alberto Arana Boza <aaranaboza@gmail.com>
2016-11-09 16:42:52 +0100

    Fixed locale

---
+++ locales/en.js
@@ -0,0 +1,61 @@
+module.exports = {
+  alert:{
+    build: 'This website is in build'
+  },
+  nav: {
+    home: 'Home',
+    about: 'About',
+    contact: 'Contact'
+  },
+  footer:{
+    with: 'with',
+    by: 'by'
+  },
+  contact:{
+    title: 'Contact',
+    subtitle: 'Thanks you for you visit',
+    contact: 'If you want to contact me, you can do so by email, aaranaboza[at]gmail.com, or by any of the following means.'
+  },
+  home:{
+    title: 'I\'m Alberto Arana',
+    subtitle: 'Web developer',
+    fText: 'I\'m a web developer. Right now, I\'m working with Javascript frameworks as AngulaJS and ReactJS for frontend and NodeJS for backend. Currently always use Sass or Stylus to CSS.',
+    sText: 'I\'m always learning new technologies and frameworks.',
+    title2: 'Main skills'
+  },
+  about:{
+    title: 'Who am I?',
+    subtitle: 'Web developer',
+    p: 'My name is Alberto Arana, I am a young computer engineer for ',
+    link: 'University of Seville',
+    p2: 'Spain. Always eager to learn and research. Currently, my main objetive is to specialize in a full-stack web developer.',
+    p3: 'Within the development, firstly I am more focused on the frontend (HTML5, CSS3, JS, Responsive Design) in development and design. However as a secondary objective, I am learning and researching in the backend (Node.js, MongoDB, Python).',
+    p4: 'Currently I have ITIL® Foundation Certificate in IT Service Management by ',
+    p5: 'I\'m a nice, cheerful and friendly person with initiative. I love to research and keep abreast of technological advances. I like going out to have fun and play sports to stay fit. Web development, cybersecurity and mobile application development are several categories that I like to keep up given the great value and importance for development and technological innovation.',
+    title2: 'About me',
+    title3: 'My skills',
+    subtitle3: 'Like developer',
+    title4: 'Work experience',
+    subtitle4: 'Business'
+  },
+  works:{
+    fidetia:{
+      position: 'Research Assistant',
+      startDate: 'March 2016',
+      endDate: 'September 2016',
+      description: 'Belonging to Fidetia research group, responsible for the development of a web application for a project in collaboration with Arion Group.'
+    },
+    scheider:{
+      position: 'Web developer',
+      startDate: 'January 2016',
+      endDate: 'February 2016',
+      description: 'Belong to the Micro group, responsible for the development and optimization of internal applications of Software department.'
+    },
+    azcatec:{
+      position: 'Computer engineer',
+      startDate: 'December 2014',
+      endDate: 'Juny 2015',
+      description: 'As the only member of the IT team, responsible for developing business software ERP and responsible for maintaining the system, network and computer equipment.'
+    }
+  }
+}
---
+++ locales/es.js
@@ -0,0 +1,61 @@
+module.exports = {
+  alert:{
+    build: 'Esta página web esta en construción'
+  },
+  nav: {
+    home: 'Inicio',
+    about: 'Sobre mi',
+    contact: 'Contacto'
+  },
+  footer:{
+    with: 'con',
+    by: 'por'
+  },
+  contact:{
+    title: 'Contacto',
+    subtitle: 'Gracias por su visita',
+    contact: 'Si tu quieres contactar conmigo, puedes hacerlo por email, aaranaboza[at]gmail.com, o por cualquiera de los siguientes medios.'
+  },
+  home:{
+    title: 'Soy Alberto Arana',
+    subtitle: 'Desarollador Web',
+    fText: 'Soy un desarrollador web. En estos momentos, estoy trabajando con frameworks de Javascript como AngulaJS y ReactJS para frontend y NodeJS para backend. Actualmente siempre uso Sass o Stylus para CSS.',
+    sText: 'Siempre estoy aprendiendo nuevas tecnologias y frameworks.',
+    title2: 'Principales habilidades'
+  },
+  about:{
+    title: '¿Quien soy?',
+    subtitle: 'Desarrolador Web',
+    p: 'Mi nombre es Alberto Arana, soy un joven ingeniero informático por la ',
+    link: 'Universidad de Sevilla',
+    p2: 'España. Siempre ansioso de aprender e investigar. Actualmente, mi principal objetivo es especializarme en un desarollador web Full-Stack.',
+    p3: 'Dentro del desarrollo web, primeramente yo estoy más enfocado en el frontend (HTML5, CSS3, JS, Responsive Design) en desarrollo y diseño. Sin embargo como objetivo secundario, yo estoy aprendiendo e investigando en el backend (Node.js, MongoDB, Python).',
+    p4: 'Actualmente dispongo de ITIL® Foundation Certificate in IT Service Management por ',
+    p5: 'Soy una persona agradable, alegre y amable con iniciativa propia. Me encanta investigar y estar al tanto de los avances tecnológicos. Me gusta salir a divertirme y hacer deporte para mantenerme en forma. El desarrollo web, la ciberseguridad y el desarrollo de aplicaciones móviles son la mayoría de categorías que más me gustan de mantenerme al día dado el gran valor e importancia para el desarrollo y la innovación tecnológica.',
+    title2: 'Sobre mi',
+    title3: 'Mis habilidades',
+    subtitle3: 'Como desarrollador',
+    title4: 'Experiencia laboral',
+    subtitle4: 'Empresas'
+  },
+  works:{
+    fidetia:{
+      position: 'Ayudante de investigación',
+      startDate: 'Marzo 2016',
+      endDate: 'Septiembre 2016',
+      description: 'Perteneciendo al grupo de investigación de Fidetia, responsable del desarrollo de una aplicación web para un proyecto en colaboración con Arion Grupo.'
+    },
+    scheider:{
+      position: 'Desarrollador web',
+      startDate: 'Enero 2016',
+      endDate: 'Febrero 2016',
+      description: 'Perteneciendo al grupo Micro, responsable del desarrollo y optimización de las aplicaciones internas del departamento de Software.'
+    },
+    azcatec:{
+      position: 'Ingeniero informático',
+      startDate: 'Diciembre 2014',
+      endDate: 'Junio 2015',
+      description: 'Como único miembro del equipo de IT, responsable del desarrollo de un software de ERP empresarial y responsable del mantenimiento del sistema, redes y equipamiento informático.'
+    }
+  }
+}
